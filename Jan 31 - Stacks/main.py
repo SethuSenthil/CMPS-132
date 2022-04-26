72096e8fc35921a61e1a6b3dd0c280494f90ac00
Sethu Narayan Senthil <github@sethusenthil.com>
2022-04-25 21:34:06 -0400

    update

---
+++ Jan 31 - Stacks/main.py
@@ -0,0 +1,14 @@
+"""
+Stack - Data Structure
+LIFO - Last In First Out
+
+top, bottom
+items are pushed at top
+items are also poped also from the top
+
+peek() vs pop():
+peek() - returns the top item without removing it aka READ ONLY
+pop() - returns the top item and removes it aka READ AND WRITE
+push() - adds an item to the top
+isEmpty() - returns True if the stack is empty
+"""
